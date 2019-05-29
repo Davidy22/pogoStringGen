0f03d5ffb7236456560c2688a04e03122bb5ac38
David Yang <davidyang6us@gmail.com>
2019-05-30 06:48:23 +0800

    progress

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from flask import Flask, jsonify, render_template, request
+import json
 app = Flask(__name__)
+
+file = open("data/dump.json")
+pokemon = json.load(file)
+pokemonList = []
+for key in pokemon:
+	pokemonList.append(pokemon[key])
+
 
 @app.route('/_add_numbers')
 def add_numbers():
-    a = request.args.get('a', 0, type=int)
-    b = request.args.get('b', 0, type=int)
-    return jsonify(result=a + b)
+	pok = request.args.getlist('pok[]')
+	total = 0
+	for i in pok:
+		total += i
+	return jsonify(result=total)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
