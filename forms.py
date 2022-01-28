e60b3bf1fcf2c007889c13bc002c74fa04e438a4
David Yang <davidyang6us@gmail.com>
2022-01-28 19:10:15 +0800

    Remove unused/outdated files

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired
-
-class ContactForm(FlaskForm):
-	name = StringField('Name', validators=[DataRequired()])
-	email = StringField('Email', validators=[DataRequired()])
-	message = TextAreaField('Message', validators=[DataRequired()])
