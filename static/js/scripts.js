789864cc585e7013671a88e50eae3bd782d6a0d6
David Yang <davidyang6us@gmail.com>
2024-04-29 19:21:55 +0800

    Declaration consistency

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,68 +1,59 @@
-$(function() {
-	$('div#import').bind('click', function() {
-		$.post($SCRIPT_ROOT + '/_import_string', {
-			pok: $("#importBox").val()
-		}, function(data) {
-			var toSelect = data.result;
-			$(".pokemon").removeClass("select")
+function importString() {
+	$.post($SCRIPT_ROOT + '/_import_string', {
+		pok: $("#importBox").val()
+	}, function(data) {
+		var toSelect = data.result;
+		$(".pokemon").removeClass("select")
 
 
-			for (i=0; i < toSelect.length; i++) {
-				$( "#" + toSelect[i] ).addClass("select")
-			}
-			getResult();
-			return false;
-		});
-	});
-});
-
-
-$(function() {
-	$('div#add').bind('click', function() {
-		$.post($SCRIPT_ROOT + '/_import_string', {
-			pok: $("#importBox").val()
-		}, function(data) {
-			var toSelect = data.result;
-
-			for (i=0; i < toSelect.length; i++) {
-				$( "#" + toSelect[i] ).addClass("select")
-			}
-			getResult();
-			return false;
-		});
-
-	});
-});
-
-
-$(function() {
-	$('div#sub').bind('click', function() {
-		$.post($SCRIPT_ROOT + '/_import_string', {
-			pok: $("#importBox").val()
-		}, function(data) {
-			var toSelect = data.result;
-
-			for (i=0; i < toSelect.length; i++) {
-				$( "#" + toSelect[i] ).removeClass("select")
-			}
-			
-			getResult();
-			return false;
-		});
-									
-	});
-});
-
-
-$(function() {
-	$('div#invert').bind('click', function() {
-		$(".pokemon").toggleClass("select")
+		for (i=0; i < toSelect.length; i++) {
+			$( "#" + toSelect[i] ).addClass("select")
+		}
 		getResult();
+		return false;
 	});
-});
+};
 
 
-function copy () {
+function addString() {
+	$.post($SCRIPT_ROOT + '/_import_string', {
+		pok: $("#importBox").val()
+	}, function(data) {
+		var toSelect = data.result;
+
+		for (i=0; i < toSelect.length; i++) {
+			$( "#" + toSelect[i] ).addClass("select")
+		}
+		getResult();
+		return false;
+	});
+};
+
+
+function subString() {
+	$.post($SCRIPT_ROOT + '/_import_string', {
+		pok: $("#importBox").val()
+	}, function(data) {
+		var toSelect = data.result;
+
+		for (i=0; i < toSelect.length; i++) {
+			$( "#" + toSelect[i] ).removeClass("select")
+		}
+		
+		getResult();
+		return false;
+	});
+								
+};
+
+
+function invertString() {
+	$(".pokemon").toggleClass("select")
+	getResult();
+};
+
+
+function copy() {
 	var copyText = document.getElementById("result");
 		copyText.select();
 		document.execCommand("Copy");
